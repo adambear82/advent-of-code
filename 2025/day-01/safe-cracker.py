e5d816e8968440bc3443144a02bd053d02866de2
Adam Bear <32963786+adambear82@users.noreply.github.com>
2025-12-19 12:39:38 +0000

    Refactor safe-cracker.py for improved counting logic
    in part 1 we solved for counting when dial landed on zero
    
    updated and solved for part 2 not just counting when dial lands on zero but every time when the when the dial passes through zero

--- 2025/day-01/safe-cracker.py
+++ 2025/day-01/safe-cracker.py
@@ -4,10 +4,26 @@ import requests
 # Get input data from GitHub, couldn't use Advent of Code site directly
 url = "https://raw.githubusercontent.com/adambear82/advent-of-code/refs/heads/main/2025/day-01/input.txt"
 resp = requests.get(url, timeout=10)
+
+LR = """
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"""
+LR = LR.strip().splitlines()
+#LR = LR[:2]
+
 LR = resp.text.strip().splitlines()
 
 # initialize variables
-counter = 0
+part1_counter, part2_counter = 0, 0
 dial = 1050
 moves = []
 
@@ -17,14 +33,45 @@ for lr in LR :
     moves.append(int(signed_str))
 
 # this print is ony useful for debugging, there are too many moves to see them all
-print ("the dial starts by pointing to", dial)
+print (f"\nthe dial starts by pointing to :\n{dial}")
+print (f"\nthe moves are :\n{moves}\n")
+# part 1 output for counting dial at rest
 
 # process each move, updating the dial and counting multiples of 100, using modulo
 for m in moves :
     dial += m
     print (dial)
     if dial % 100 == 0 :
-        counter += 1
+        part1_counter += 1
 
 # final output
-print("The dial landed on a multiple of 100 a total of", counter, "times.")
+print(f"\nThe dial landed on a multiple of 100 a total of {part1_counter} times.\n")
+
+
+# part 2 output for counting dial in motion
+# process each move, updating the dial and counting multiples of 100, using modulo
+for m in moves :
+    print("-" * 20)
+    print(m)
+    if m < 0 :
+        print("negative\n")
+        clicks = [-1 for _ in range(abs(m))]
+    else :
+        print("positive\n")
+        clicks = [1 for _ in range(m)]
+    print(clicks)
+
+    for c in clicks :
+        dial += c
+        print (dial)
+        if dial % 100 == 0 :
+            part2_counter += 1
+            print("*" * 5, part2_counter)
+
+
+# final output
+print("-" * 20)
+print(f"\nThe dial landed on a multiple of 100 a total of {part1_counter} times.\n")
+
+print(f"\nThe dial passed through a multiple of 100 a total of {part2_counter} times.\n")
+

22ef02303c9f9923cc50254c8f86722ab0ae1701
Adam Bear <32963786+adambear82@users.noreply.github.com>
2026-03-26 09:08:22 +0000

    Refactor triangle validation and clean up code

--- 2016/day-03/count_tri.py
+++ 2016/day-03/count_tri.py
@@ -1,13 +1,14 @@
 # -------- boilerplate -------- 
 
 import sys, os#, itertools
-from tabulate import tabulate # sudo apt install python3-tabulate
+# from tabulate import tabulate # sudo apt install python3-tabulate
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils.vibes import get_raw, part, wipe, dash, dec_print, prints
+from utils.vibes import get_raw, part, wipe# dash, dec_print, prints
 
 # --------- definitions ---------
 
-
+def is_triangle(a, b, c):
+    return a + b > c and a + c > b and b + c > a
 
 # -------- main -----------------
 
@@ -17,34 +18,27 @@ part(8)
 triangles = get_raw(day=3, year=2016, file_name="input.txt")
 triangles = triangles.strip().split("\n")
 triangles = [i.split() for i in triangles]
+triangles = [[int(j) for j in i] for i in triangles]
 print(triangles[:4], "...")
 
 part(9)
-tri_sum = int(triangles[0][0]) + int(triangles[0][1])
-boo = tri_sum > int(triangles[0][2])
-print(f"{triangles[0][0]} + {triangles[0][1]} = {tri_sum} > {triangles[0][2]}, {boo} triangle")
 
+tri_sum_0 = triangles[0][0] + triangles[0][1]
+tri_sum_3 = triangles[3][2] + triangles[3][1]
+boo_0 = tri_sum_0 > triangles[0][2]
+boo_3 = tri_sum_3 > triangles[3][0]
+print(f"{triangles[0][0]} + {triangles[0][1]} = {tri_sum_0} > {triangles[0][2]}, {boo_0} triangle")
+print(f"{triangles[3][2]} + {triangles[3][1]} = {tri_sum_3} > {triangles[3][0]}, {boo_3} triangle")
 
 part(1)
 
 counter = 0
 for a, b, c in triangles :
-    a = int(a)
-    b = int(b)
-    c = int(c)
-    if a + b > c and a + c > b and b + c > a :
+    if is_triangle(a, b, c) :
         counter += 1
 print(f"true triangles: {counter}")
 
 part(2)
-
-
-def is_triangle(a, b, c):
-    a = int(a)
-    b = int(b)
-    c = int(c)
-    return a + b > c and a + c > b and b + c > a
-
 counter = 0
 
 # Process input in chunks of 3 rows
@@ -57,5 +51,5 @@ for i in range(0, len(triangles), 3):
         if is_triangle(a, b, c):
             counter += 1
 
-print("Column-wise true triangles:", counter)
+print(f"Column-wise true triangles: {counter}\n")
 
